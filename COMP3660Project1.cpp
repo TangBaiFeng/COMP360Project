8a5253d24eac0886c39a4b1052ae9d299788bd3d
Tang <troy1636@gmail.com>
2021-09-17 19:48:20 -0400

    shit

--- COMP3660Project1.cpp
+++ COMP3660Project1.cpp
@@ -1,12 +1,32 @@
-// COMP3660Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+/**
+ * @brief We will input a file, indentify the substrings and find all syntax errors
+ * 
+ */
+#include<iostream>
 using namespace std;
-int main()
+/**
+ * @brief Lexical Analyzer to generate lexemes and tokens, and display them.
+ * 
+ */
+void lexicalAnalyzer();
+/**
+ * @brief recursive-descent parser to check the syntax of the test program.
+ * 
+ */
+void topDownParser();
+int main(int argc, char const *argv[])
 {
-	cout << "Hello World!\n";
+    /* code */
+    return 0;
 }
+
+void lexicalAnalyzer(){
+
+}
+void topDownParser(){
+    
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
@@ -18,4 +38,3 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-f you
